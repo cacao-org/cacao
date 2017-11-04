e1adebc2c22024985a7d45902d19edb00a3f93ae
nour-sf <31612291+nour-sf@users.noreply.github.com>
2017-11-03 16:26:59 -1000

    sync

---
+++ src/CLImain.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <CommandLineInterface/CLIcore.h>
+
+
+int main(int argc, char *argv[])
+{
+
+	printf("WELCOME to nipTK\n\n");
+	runCLI(argc, argv);
+
+
+	return 0;
+}
