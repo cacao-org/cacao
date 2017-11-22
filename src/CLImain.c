86696508b8c9aecf99d8f95b9413ae392617f143
oguyon <oliv.guyon@gmail.com>
2017-11-21 21:26:42 -1000

    sync

--- src/CLImain.c
+++ src/CLImain.c
@@ -3,9 +3,18 @@
 #include <CommandLineInterface/CLIcore.h>
 
 
+#define STYLE_BOLD    "\033[1m"
+#define STYLE_NO_BOLD "\033[22m"
+
+
+
 int main(int argc, char *argv[])
 {
 	char *AppName = "cacao";
+
+	printf(STYLE_BOLD);
+	printf("\n        Compute And Control for Adaptive Optics (cacao)\n");
+	printf(STYLE_NO_BOLD);
 
 	runCLI(argc, argv, AppName);
 
