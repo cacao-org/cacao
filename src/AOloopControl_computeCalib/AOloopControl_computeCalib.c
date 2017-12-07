f72bdd004bb77f8acd0a010d10dcdf8374839449
nour-sf <nour.skaf@gmail.com>
2017-12-06 17:00:46 -1000

    var org

--- src/AOloopControl_computeCalib/AOloopControl_computeCalib.c
+++ src/AOloopControl_computeCalib/AOloopControl_computeCalib.c
@@ -130,7 +130,7 @@ extern AOLOOPCONTROL_CONF *AOconf; // declared in AOloopControl.c
 extern AOloopControl_var aoloopcontrol_var; // declared in AOloopControl.c
 extern int AOloopcontrol_meminit;
 
-
+long aoconfID_imWFS2_active[100];
 
 
 
