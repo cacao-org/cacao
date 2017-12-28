ed1b6ac24f5a72c068ca8ffc6f2b8f8552bc79f0
nour-sf <nour.skaf@gmail.com>
2017-12-27 22:24:51 -1000

    clean

--- src/AOloopControl/AOloopControl_wfs.c
+++ src/AOloopControl/AOloopControl_wfs.c
@@ -19,33 +19,12 @@
 
 
 
-#ifdef __MACH__   // for Mac OS X - 
-//#include <mach/mach_time.h>
-//#define CLOCK_REALTIME 0
-//#define CLOCK_MONOTONIC 0
-int clock_gettime(int clk_id, struct mach_timespec *t) {
-    mach_timebase_info_data_t timebase;
-    mach_timebase_info(&timebase);
-    uint64_t time;
-    time = mach_absolute_time();
-    double nseconds = ((double)time * (double)timebase.numer)/((double)timebase.denom);
-    double seconds = ((double)time * (double)timebase.numer)/((double)timebase.denom * 1e9);
-    t->tv_sec = seconds;
-    t->tv_nsec = nseconds;
-    return 0;
-}
-#else
-#include <time.h>
-#endif
-
-
 #include <string.h>
-#include <gsl/gsl_math.h>
 #include <gsl/gsl_blas.h>
 #include <pthread.h>
 #include "info/info.h" 
 
-//libraries created by O. Guyon 
+
 #include "CommandLineInterface/CLIcore.h"
 #include "AOloopControl/AOloopControl.h"
 #include "00CORE/00CORE.h"
