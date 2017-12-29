8caedb279bcb33b220ab3ae7eeb6898e999de2f1
oguyon <oliv.guyon@gmail.com>
2017-12-28 22:02:11 -1000

    msync

--- src/FPAOloopControl/FPAOloopControl.c
+++ src/FPAOloopControl/FPAOloopControl.c
@@ -199,7 +199,12 @@ int_fast8_t FPAOloopControl_MakeLinComb_seq_cli(){
 void __attribute__ ((constructor)) libinit_FPAOloopControl()
 {
 	init_FPAOloopControl();
-//	printf(" ...... Loading module %s\n", __FILE__);
+
+	if(data.progStatus>0)
+	{
+		printf("  Found unloaded shared object in ./libs/ -> LOADING module %s\n", __FILE__);
+		fflush(stdout);
+	}	
 }
 
 
