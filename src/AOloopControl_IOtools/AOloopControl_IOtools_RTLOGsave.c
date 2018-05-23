9c09810f1740b7830868afefbfcddf5439b79177
guyon <oliv.guyon@gmail.com>
2018-05-23 01:08:03 -1000

    RTLOG save

--- src/AOloopControl_IOtools/AOloopControl_IOtools_RTLOGsave.c
+++ src/AOloopControl_IOtools/AOloopControl_IOtools_RTLOGsave.c
@@ -42,10 +42,12 @@ int_fast8_t AOloopControl_IOtools_RTLOGsave(long loop, const char *streamname, c
 	// data buffers
 	char imnameb0[500];
     char imnameb1[500];
+	long IDinb0, IDinb1;
 	
 	// info buffers
 	char imnamebinfo0[500];
     char imnamebinfo1[500];
+	long IDinbinfo0, IDinbinfo1;
 	
 
     if(sprintf(imnameb0, "aol%ld_%s_logbuff0", loop, streamname) < 1)
@@ -60,9 +62,24 @@ int_fast8_t AOloopControl_IOtools_RTLOGsave(long loop, const char *streamname, c
     if(sprintf(imnamebinfo1, "aol%ld_%s_logbuffinfo1", loop, streamname) < 1)
         printERROR(__FILE__, __func__, __LINE__, "sprintf wrote <1 char");
 
+   printf("DATA buffers:  %s  %s\n", imnameb0, imnameb1);
+   printf("INFO buffers:  %s  %s\n", imnamebinfo0, imnamebinfo1);
    
-   
-   
-   
+    IDinb0 = read_sharedmem_image(imnameb0);
+    IDinb1 = read_sharedmem_image(imnameb1);
+    IDinbinfo0 = read_sharedmem_image(imnamebinfo0);
+    IDinbinfo1 = read_sharedmem_image(imnamebinfo1);
+
+	list_image_ID();
+
+/*    cnt0_old = data.image[IDinb0].md[0].cnt0;
+    cnt1_old = data.image[IDinb1].md[0].cnt0;
+
+    xsize = data.image[IDinb0].md[0].size[0];
+    ysize = data.image[IDinb0].md[0].size[1];
+    xysize = xsize*ysize;
+    zsize = data.image[IDinb0].md[0].size[2];
+    atype = data.image[IDinb0].md[0].atype;
+  */ 
     return(0);
 }
