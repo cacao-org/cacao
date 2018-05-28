3c85725e1acdee89803835698b414844d5082242
guyon <oliv.guyon@gmail.com>
2018-05-27 15:17:30 -1000

    new RTlog format

--- src/AOloopControl_perfTest/AOloopControl_perfTest_status.c
+++ src/AOloopControl_perfTest/AOloopControl_perfTest_status.c
@@ -198,7 +198,6 @@ int_fast8_t AOloopControl_perfTest_printloopstatus(long loop, long nbcol, long I
     printw("    DMprimWrite = %d   Predictive control state: %d        ARPF gain = %5.3f   AUTOTUNE LIM = %d (perc = %.2f %%  delta = %.3f nm mcoeff=%4.2f) GAIN = %d\n", AOconf[loop].DMprimaryWriteON, AOconf[loop].ARPFon, AOconf[loop].ARPFgain, AOconf[loop].AUTOTUNE_LIMITS_ON, AOconf[loop].AUTOTUNE_LIMITS_perc, 1000.0*AOconf[loop].AUTOTUNE_LIMITS_delta, AOconf[loop].AUTOTUNE_LIMITS_mcoeff, AOconf[loop].AUTOTUNE_GAINS_ON);
     printw(" TIMIMNG :  lfr = %9.3f Hz    hw lat = %5.3f fr   comp lat = %5.3f fr  wfs extr lat = %5.3f fr\n", AOconf[loop].loopfrequ, AOconf[loop].hardwlatency_frame, AOconf[loop].complatency_frame, AOconf[loop].wfsmextrlatency_frame);
     printw("loop iteration CNT : %lld   ", AOconf[loop].cnt);
-	printw("RTLOG:%d  modeval_ol:%d\n", AOconf[loop].RTLOG_ON, AOconf[loop].RTstreamLOG_modeval_ol_ON);
     printw("\n");
 
 
