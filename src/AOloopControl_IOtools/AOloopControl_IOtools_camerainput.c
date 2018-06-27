113c9e414e81ae2c7295eedc34de5f3840c20a0a
guyon <oliv.guyon@gmail.com>
2018-06-26 14:59:12 -1000

    added logging

--- src/AOloopControl_IOtools/AOloopControl_IOtools_camerainput.c
+++ src/AOloopControl_IOtools/AOloopControl_IOtools_camerainput.c
@@ -407,7 +407,7 @@ static void *compute_function_dark_subtract( void *ptr )
 	int logfunc_level = 0;
 	int logfunc_level_max = 1;
 	char commentstring[200];
-	sprintf(commentstring, "Dark subtract WFS image, loop %ld", loop);
+	sprintf(commentstring, "Dark subtract WFS image, loop %ld", LOOPNUMBER);
 	CORE_logFunctionCall( logfunc_level, logfunc_level_max, 0, __FILE__, __func__, __LINE__, commentstring);
 
 
