b21b7615f1d72e296b26485740ff08d6e4503134
guyon <oliv.guyon@gmail.com>
2018-07-21 10:31:34 -1000

    added .c files

---
+++ src/AOloopControl/AOloopControl_CompModes_loop.c
@@ -0,0 +1,221 @@
+/**
+ * @file    AOloopControl_aorun.c 
+ * @brief   AO loop Control compute functions 
+ * 
+ * REAL TIME COMPUTING ROUTINES
+ *  
+ * @author  O. Guyon
+ * @date    2018-01-04
+ *
+ * 
+ * @bug No known bugs.
+ * 
+ */
+
+
+
+#define _GNU_SOURCE
+
+// uncomment for test print statements to stdout
+//#define _PRINT_TEST
+
+
+#include <time.h>
+
+
+
+#include <string.h>
+#include <sched.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_blas.h>
+#include <pthread.h>
+#include "info/info.h" 
+
+//libraries created by O. Guyon 
+#include "CommandLineInterface/CLIcore.h"
+#include "AOloopControl/AOloopControl.h"
+#include "00CORE/00CORE.h"
+#include "COREMOD_memory/COREMOD_memory.h"
+#include "COREMOD_iofits/COREMOD_iofits.h"
+#include "AOloopControl_IOtools/AOloopControl_IOtools.h"
+
+#ifdef HAVE_CUDA
+#include "cudacomp/cudacomp.h"
+#endif
+
+# ifdef _OPENMP
+# include <omp.h>
+#define OMP_NELEMENT_LIMIT 1000000
+# endif
+
+#define NB_AOloopcontrol 10 // max number of loops
+
+static int AOlooploadconf_init = 0;
+
+
+
+
+extern AOLOOPCONTROL_CONF *AOconf; // configuration - this can be an array
+extern AOloopControl_var aoloopcontrol_var;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int_fast8_t AOloopControl_CompModes_loop(const char *ID_CM_name, const char *ID_WFSref_name, const char *ID_WFSim_name, const char *ID_WFSimtot_name, const char *ID_coeff_name)
+{
+#ifdef HAVE_CUDA
+
+    int *GPUsetM;
+    long ID_CM;
+    long ID_WFSref;
+    long ID_coeff;
+    long GPUcnt;
+    int k;
+    int_fast8_t GPUstatus[100];
+    int_fast8_t status;
+    long NBmodes;
+    uint32_t *sizearray;
+
+    long ID_WFSim;
+    long ID_WFSim_n;
+    long wfsxsize, wfsysize;
+    int m;
+    long IDcoeff0;
+
+    long ID_WFSimtot;
+    double totfluxave;
+    long ID_coefft;
+
+    double alpha = 0.1;
+	char imname[200];
+
+
+
+	// LOG function start
+	int logfunc_level = 0;
+	int logfunc_level_max = 1;
+	char commentstring[200];
+	sprintf(commentstring, "loop %ld", aoloopcontrol_var.LOOPNUMBER);
+	CORE_logFunctionCall( logfunc_level, logfunc_level_max, 0, __FILE__, __func__, __LINE__, commentstring);
+	
+	
+	
+	if(aoloopcontrol_var.aoconfID_looptiming == -1)
+	{
+		// LOOPiteration is written in cnt1 of loop timing array
+		if(sprintf(imname, "aol%ld_looptiming", aoloopcontrol_var.LOOPNUMBER) < 1)
+			printERROR(__FILE__, __func__, __LINE__, "sprintf wrote <1 char");
+		aoloopcontrol_var.aoconfID_looptiming = AOloopControl_IOtools_2Dloadcreate_shmim(imname, " ", aoloopcontrol_var.AOcontrolNBtimers, 1, 0.0);
+	}
+
+
+    GPUcnt = 2;
+
+    GPUsetM = (int*) malloc(sizeof(int)*GPUcnt);
+    for(k=0; k<GPUcnt; k++)
+        GPUsetM[k] = k+5;
+
+
+    ID_CM = image_ID(ID_CM_name);
+    wfsxsize = data.image[ID_CM].md[0].size[0];
+    wfsysize = data.image[ID_CM].md[0].size[1];
+    NBmodes = data.image[ID_CM].md[0].size[2];
+
+    ID_WFSref = image_ID(ID_WFSref_name);
+
+
+    sizearray = (uint32_t*) malloc(sizeof(uint32_t)*2);
+    sizearray[0] = NBmodes;
+    sizearray[1] = 1;
+
+    ID_coeff = create_image_ID(ID_coeff_name, 2, sizearray, _DATATYPE_FLOAT, 1, 0);
+    COREMOD_MEMORY_image_set_createsem(ID_coeff_name, 10);
+    data.image[ID_coeff].md[0].cnt0 = 0;
+
+    ID_coefft = create_image_ID("coefftmp", 2, sizearray, _DATATYPE_FLOAT, 1, 0);
+    COREMOD_MEMORY_image_set_createsem("coefftmp", 10);
+
+
+    IDcoeff0 = create_image_ID("coeff0", 2, sizearray, _DATATYPE_FLOAT, 1, 0);
+    ID_WFSim_n = create_2Dimage_ID("wfsim_n", wfsxsize, wfsysize);
+    COREMOD_MEMORY_image_set_createsem("wfsim_n", 10);
+
+
+
+
+    ID_WFSim = image_ID(ID_WFSim_name);
+    ID_WFSimtot = image_ID(ID_WFSimtot_name);
+
+
+    GPU_loop_MultMat_setup(2, ID_CM_name, "wfsim_n", "coefftmp", GPUcnt, GPUsetM, 0, 1, 1, 0);
+
+    totfluxave = 1.0;
+    int initWFSref;
+    for(;;)
+    {
+        if(initWFSref==0)
+        {
+            printf("Computing reference\n");
+            fflush(stdout);
+            memcpy(data.image[ID_WFSim_n].array.F, data.image[ID_WFSref].array.F, sizeof(float)*wfsxsize*wfsysize);
+            GPU_loop_MultMat_execute(2, &status, &GPUstatus[0], 1.0, 0.0, 0, 0);
+            for(m=0; m<NBmodes; m++)
+            {
+                data.image[IDcoeff0].array.F[m] = data.image[ID_coefft].array.F[m];
+            }
+            printf("\n");
+            initWFSref = 1;
+            printf("reference computed\n");
+            fflush(stdout);
+        }
+
+        memcpy(data.image[ID_WFSim_n].array.F, data.image[ID_WFSim].array.F, sizeof(float)*wfsxsize*wfsysize);
+        COREMOD_MEMORY_image_set_semwait(ID_WFSim_name, 0);
+
+        GPU_loop_MultMat_execute(2, &status, &GPUstatus[0], 1.0, 0.0, 0, 0);
+        totfluxave = (1.0-alpha)*totfluxave + alpha*data.image[ID_WFSimtot].array.F[0];
+
+        data.image[ID_coeff].md[0].write = 1;
+        for(m=0; m<NBmodes; m++)
+            data.image[ID_coeff].array.F[m] = data.image[ID_coefft].array.F[m]/totfluxave - data.image[IDcoeff0].array.F[m];
+        data.image[ID_coeff].md[0].cnt0 ++;
+        data.image[ID_coeff].md[0].cnt1 = data.image[aoloopcontrol_var.aoconfID_looptiming].md[0].cnt1;
+        data.image[ID_coeff].md[0].write = 0;
+    }
+
+
+    delete_image_ID("coeff0");
+    free(sizearray);
+
+    free(GPUsetM);
+
+
+	// LOG function start
+	CORE_logFunctionCall( logfunc_level, logfunc_level_max, 1, __FILE__, __func__, __LINE__, commentstring);
+
+
+#endif
+
+    return(0);
+}
+
+
+
+
+
---
+++ src/AOloopControl/AOloopControl_sig2Modecoeff.c
@@ -0,0 +1,146 @@
+/**
+ * @file    AOloopControl_aorun.c 
+ * @brief   AO loop Control compute functions 
+ * 
+ * REAL TIME COMPUTING ROUTINES
+ *  
+ * @author  O. Guyon
+ * @date    2018-01-04
+ *
+ * 
+ * @bug No known bugs.
+ * 
+ */
+
+
+
+#define _GNU_SOURCE
+
+// uncomment for test print statements to stdout
+//#define _PRINT_TEST
+
+
+#include <time.h>
+
+
+
+#include <string.h>
+#include <sched.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_blas.h>
+#include <pthread.h>
+#include "info/info.h" 
+
+//libraries created by O. Guyon 
+#include "CommandLineInterface/CLIcore.h"
+#include "AOloopControl/AOloopControl.h"
+#include "00CORE/00CORE.h"
+#include "COREMOD_memory/COREMOD_memory.h"
+#include "COREMOD_iofits/COREMOD_iofits.h"
+#include "AOloopControl_IOtools/AOloopControl_IOtools.h"
+
+
+
+
+
+
+
+
+
+extern AOLOOPCONTROL_CONF *AOconf; // configuration - this can be an array
+extern AOloopControl_var aoloopcontrol_var;
+
+
+
+
+
+
+
+
+
+
+
+//
+// assumes the WFS mode basis is already orthogonal
+// removes reference from each frame
+//
+long AOloopControl_sig2Modecoeff(const char *WFSim_name, const char *IDwfsref_name, const char *WFSmodes_name, const char *outname)
+{
+    long IDout;
+    long IDwfs, IDmodes, IDwfsref;
+    long wfsxsize, wfsysize, wfssize, NBmodes, NBframes;
+    double totref;
+    float coeff;
+    long ii, m, kk;
+    FILE *fp;
+    double *mcoeff_ave;
+    double *mcoeff_rms;
+
+
+    IDwfs = image_ID(WFSim_name);
+    wfsxsize = data.image[IDwfs].md[0].size[0];
+    wfsysize = data.image[IDwfs].md[0].size[1];
+    NBframes = data.image[IDwfs].md[0].size[2];
+    wfssize = wfsxsize*wfsysize;
+
+
+
+
+    IDwfsref = image_ID(IDwfsref_name);
+
+    IDmodes = image_ID(WFSmodes_name);
+    NBmodes = data.image[IDmodes].md[0].size[2];
+
+    mcoeff_ave = (double*) malloc(sizeof(double)*NBmodes);
+    mcoeff_rms = (double*) malloc(sizeof(double)*NBmodes);
+
+
+
+    IDout = create_2Dimage_ID(outname, NBframes, NBmodes);
+
+    totref = 0.0;
+
+    for(ii=0; ii<wfssize; ii++)
+        totref += data.image[IDwfsref].array.F[ii];
+    for(ii=0; ii<wfssize; ii++)
+        data.image[IDwfsref].array.F[ii] /= totref;
+
+    for(kk=0; kk<NBframes; kk++)
+    {
+		double totim = 0.0;
+		
+        for(ii=0; ii<wfssize; ii++)
+            totim += data.image[IDwfs].array.F[kk*wfssize+ii];
+        for(ii=0; ii<wfssize; ii++)
+        {
+            data.image[IDwfs].array.F[kk*wfssize+ii] /= totim;
+            data.image[IDwfs].array.F[kk*wfssize+ii] -= data.image[IDwfsref].array.F[ii];
+        }
+
+
+        for(m=0; m<NBmodes; m++)
+        {
+            coeff = 0.0;
+            for(ii=0; ii<wfssize; ii++)
+                coeff += data.image[IDmodes].array.F[m*wfssize+ii] * data.image[IDwfs].array.F[kk*wfssize+ii];
+            data.image[IDout].array.F[m*NBframes+kk] = coeff;
+            mcoeff_ave[m] += coeff;
+            mcoeff_rms[m] += coeff*coeff;
+        }
+    }
+
+
+    fp  = fopen("mode_stats.txt", "w");
+    for(m=0; m<NBmodes; m++)
+    {
+        mcoeff_rms[m] = sqrt( mcoeff_rms[m]/NBframes );
+        mcoeff_ave[m] /= NBframes;
+        fprintf(fp, "%4ld  %12g %12g\n", m, mcoeff_ave[m], mcoeff_rms[m]);
+    }
+    fclose(fp);
+
+    free(mcoeff_ave);
+    free(mcoeff_rms);
+
+    return(IDout);
+}
