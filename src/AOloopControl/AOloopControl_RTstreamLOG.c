c51817f9210330be114cf2baf6622bd6ddfab8bf
guyon <oliv.guyon@gmail.com>
2018-08-29 18:49:48 -1000

    added support for non float types

--- src/AOloopControl/AOloopControl_RTstreamLOG.c
+++ src/AOloopControl/AOloopControl_RTstreamLOG.c
@@ -1093,7 +1093,7 @@ int AOloopControl_RTstreamLOG_saveloop(int loop, char *dirname)
 							printf("Single offset %ld\n", AOconf[loop].RTSLOGarray[rtlindex].memsize*AOconf[loop].RTSLOGarray[rtlindex].SIZE);
 							printf("OFFSET    =  %ld\n", AOconf[loop].RTSLOGarray[rtlindex].FileBuffer*AOconf[loop].RTSLOGarray[rtlindex].memsize*AOconf[loop].RTSLOGarray[rtlindex].SIZE);
 							
-							printf("BUFF SIZE =  %ld\n", 2*data.image[ID].md[0].size[0]*data.image[ID].md[0].size[1]*AOconf[loop].RTSLOGarray[rtlindex].SIZE*AOconf[loop].RTSLOGarray[rtlindex].NBFileBuffer);
+							printf("BUFF SIZE =  %ld\n", 4*data.image[ID].md[0].size[0]*data.image[ID].md[0].size[1]*AOconf[loop].RTSLOGarray[rtlindex].SIZE*AOconf[loop].RTSLOGarray[rtlindex].NBFileBuffer);
 							
 							printf("END       =  %ld\n", AOconf[loop].RTSLOGarray[rtlindex].FileBuffer*AOconf[loop].RTSLOGarray[rtlindex].memsize*AOconf[loop].RTSLOGarray[rtlindex].SIZE + AOconf[loop].RTSLOGarray[rtlindex].memsize*AOconf[loop].RTSLOGarray[rtlindex].SIZE);
 							// AOconf[loop].RTSLOGarray[rtlindex].FileBuffer*
