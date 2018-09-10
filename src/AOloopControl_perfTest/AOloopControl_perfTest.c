e492e1d668f703ad069afb7b67822ab53b39cbb7
guyon <oliv.guyon@gmail.com>
2018-09-10 11:53:42 -1000

    telemetry analysis

--- src/AOloopControl_perfTest/AOloopControl_perfTest.c
+++ src/AOloopControl_perfTest/AOloopControl_perfTest.c
@@ -2093,7 +2093,7 @@ int AOloopControl_perfTest_mkSyncStreamFiles2(
 	fprintf(fp, "# dt      : %f\n", dt);
 	fprintf(fp, "#\n");
 	fprintf(fp, "# stream0 median exp time : %6.3f frame -> %8.3f Hz\n", medianexptimearray[0], medianexptimearray[0]/dt);
-	fprintf(fp, "# stream1 median exp time : %6.3f frame -> %8.3f Hz\n", medianexptimearray[1], medianexptimearray[0]/dt);
+	fprintf(fp, "# stream1 median exp time : %6.3f frame -> %8.3f Hz\n", medianexptimearray[1], medianexptimearray[1]/dt);
 	fprintf(fp, "#\n");
 	fprintf(fp, "#\n");
 	fprintf(fp, "# col 1 :   time step\n");
