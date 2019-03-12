273f42c33102ee7c29f81ab6aaf7e9423c3f99f5
Olivier Guyon <oliv.guyon@gmail.com>
2019-03-12 10:53:46 -1000

    added ImageStruct version

--- src/CLImain.c
+++ src/CLImain.c
@@ -60,6 +60,7 @@ int main(int argc, char *argv[])
 
     printf("\n");
     printf("        %s version %s\n", data.package_name, data.package_version);
+    printf("        IMAGESTRUCT_VERSION %s\n", IMAGESTRUCT_VERSION);
     printf("        GNU General Public License v3.0\n");
     printf("        Report bugs to : %s\n", PACKAGE_BUGREPORT);
     printf("        Type \"help\" for instructions\n");
