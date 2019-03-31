40803d45cbe0ea3d66862c012b501b877c116150
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2019-03-31 11:18:07 +0200

    update mkrootdirs script

--- mkrootdirs.sh
+++ mkrootdirs.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-# Check if the user is ROOT
-# if [ $(id -u) -ne 0 ]
-# then
-#     echo "You are not ROOT! Please login as ROOT."
-#     exit
-# fi
+#Check if the user is ROOT
+if [ $(id -u) -ne 0 ]
+then
+    echo ""
+    echo "  Usage: $0 <MILK_ROOT path>"
+    echo ""
+    echo "This script creates the directories's architecture needed by MILK under: (ordered by priority)"
+    echo " - \$MILK_ROOT is this variable is defined"
+    echo " - <MILK_ROOT path> put as argument"
+    echo " - /milk otherwhise"
+    echo ""
+    echo "You are not ROOT! Please login as ROOT (or sudo the command)."
+    echo ""
+    exit
+fi
 
 if [ -z $MILK_ROOT ]
 then
@@ -19,7 +28,7 @@ fi
 
 echo "Create MILK directories under $MILK_ROOT"
 echo ""
-sudo mkdir -p $MILK_ROOT
+mkdir -p $MILK_ROOT
 
 MILK_SHM_DIR=$MILK_ROOT/shm
 
@@ -34,11 +43,12 @@ echo ""
 for dir in "bin" "etc" "proc" "share" "shm" "tmp"; 
 do 
     echo "creating $MILK_ROOT/$dir"
-    sudo mkdir -p $MILK_ROOT/$dir
+    mkdir -p $MILK_ROOT/$dir
 done;
+chmod a+rwx $MILK_ROOT
 echo ""
 
 echo "initializing $MILK_SHM_DIR as tmpfs"
-sudo tee -a /etc/fstab <<< "tmpfs $MILK_SHM_DIR tmpfs rw,nosuid,nodev"
-sudo mount $MILK_SHM_DIR
+tee -a /etc/fstab <<< "tmpfs $MILK_SHM_DIR tmpfs rw,nosuid,nodev"
+mount $MILK_SHM_DIR
 
