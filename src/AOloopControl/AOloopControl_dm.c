e48dde3c47d568036155d244a124f8fd3a89d23a
Olivier Guyon <oliv.guyon@gmail.com>
2019-05-14 16:08:33 -1000

    post all semaphores

--- src/AOloopControl/AOloopControl_dm.c
+++ src/AOloopControl/AOloopControl_dm.c
@@ -124,12 +124,15 @@ int_fast8_t set_DM_modes(long loop)
 
         data.image[aoloopcontrol_var.aoconfID_dmC].md[0].write = 1;
         memcpy (data.image[aoloopcontrol_var.aoconfID_dmC].array.F, arrayf, sizeof(float)*AOconf[loop].DMctrl.sizeDM);
+        
+        COREMOD_MEMORY_image_set_sempost_byID(aoloopcontrol_var.aoconfID_dmC, -1);
+        /*
         if(data.image[aoloopcontrol_var.aoconfID_dmC].md[0].sem > 0)
         {
             sem_getvalue(data.image[aoloopcontrol_var.aoconfID_dmC].semptr[0], &semval);
             if(semval<SEMAPHORE_MAXVAL)
                 sem_post(data.image[aoloopcontrol_var.aoconfID_dmC].semptr[0]);
-        }
+        }*/
         data.image[aoloopcontrol_var.aoconfID_dmC].md[0].cnt0++;
 		data.image[aoloopcontrol_var.aoconfID_dmC].md[0].cnt1 = AOconf[loop].aorun.LOOPiteration;
         data.image[aoloopcontrol_var.aoconfID_dmC].md[0].write = 0;
@@ -152,6 +155,8 @@ int_fast8_t set_DM_modes(long loop)
 #endif
     }
 
+
+	// post semaphores on DM
     if(aoloopcontrol_var.aoconfID_dmdisp!=-1)
         if(data.image[aoloopcontrol_var.aoconfID_dmdisp].md[0].sem > 1)
         {
