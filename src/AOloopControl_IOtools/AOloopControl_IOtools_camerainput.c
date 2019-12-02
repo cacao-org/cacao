8288cd912970a37900f0cb66bb742cc1787b8706
Olivier Guyon <oliv.guyon@gmail.com>
2019-12-02 02:33:55 -1000

    added source entries

--- src/AOloopControl_IOtools/AOloopControl_IOtools_camerainput.c
+++ src/AOloopControl_IOtools/AOloopControl_IOtools_camerainput.c
@@ -547,7 +547,8 @@ errno_t AOcontrolLoop_IOtools_acquireWFSloop_FPCONF(
     // SETUP FPS
     // ===========================
     FUNCTION_PARAMETER_STRUCT fps = function_parameter_FPCONFsetup(fpsname, CMDmode, &loopstatus);
-
+	strncpy(fps.md->sourcefname, __FILE__, FPS_SRCDIR_STRLENMAX);
+	fps.md->sourceline = __LINE__;
 
     // ===========================
     // ALLOCATE FPS ENTRIES IF NOT ALREADY EXIST
