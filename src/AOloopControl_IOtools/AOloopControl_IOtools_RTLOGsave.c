28678825045bed908e6a082c44db2c0bacb6d5ae
oguyon <oliv.guyon@gmail.com>
2020-04-14 17:29:34 -1000

    use macro

--- src/AOloopControl_IOtools/AOloopControl_IOtools_RTLOGsave.c
+++ src/AOloopControl_IOtools/AOloopControl_IOtools_RTLOGsave.c
@@ -42,49 +42,45 @@ errno_t AOloopControl_IOtools_RTLOGsave(
 )
 {
     // data buffers
-	char imnameb0[500];
-    char imnameb1[500];
-	__attribute__((unused)) imageID IDinb0;
-	__attribute__((unused)) imageID IDinb1;
-	
-	// info buffers
-	char imnamebinfo0[500];
-    char imnamebinfo1[500];
-	__attribute__((unused)) imageID IDinbinfo0;
-	__attribute__((unused)) imageID IDinbinfo1;
-	
+    char imnameb0[STRINGMAXLEN_IMGNAME];
+    char imnameb1[STRINGMAXLEN_IMGNAME];
+    __attribute__((unused)) imageID IDinb0;
+    __attribute__((unused)) imageID IDinb1;
 
-    if(sprintf(imnameb0, "aol%ld_%s_logbuff0", loop, streamname) < 1)
-        printERROR(__FILE__, __func__, __LINE__, "sprintf wrote <1 char");
+    // info buffers
+    char imnamebinfo0[STRINGMAXLEN_IMGNAME];
+    char imnamebinfo1[STRINGMAXLEN_IMGNAME];
+    __attribute__((unused)) imageID IDinbinfo0;
+    __attribute__((unused)) imageID IDinbinfo1;
 
-    if(sprintf(imnameb1, "aol%ld_%s_logbuff1", loop, streamname) < 1)
-        printERROR(__FILE__, __func__, __LINE__, "sprintf wrote <1 char");
 
-    if(sprintf(imnamebinfo0, "aol%ld_%s_logbuffinfo0", loop, streamname) < 1)
-        printERROR(__FILE__, __func__, __LINE__, "sprintf wrote <1 char");
+    WRITE_IMAGENAME(imnameb0, "aol%ld_%s_logbuff0", loop, streamname);
 
-    if(sprintf(imnamebinfo1, "aol%ld_%s_logbuffinfo1", loop, streamname) < 1)
-        printERROR(__FILE__, __func__, __LINE__, "sprintf wrote <1 char");
+    WRITE_IMAGENAME(imnameb1, "aol%ld_%s_logbuff1", loop, streamname);
 
-   printf("DATA buffers:  %s  %s\n", imnameb0, imnameb1);
-   printf("INFO buffers:  %s  %s\n", imnamebinfo0, imnamebinfo1);
-   
+    WRITE_IMAGENAME(imnamebinfo0, "aol%ld_%s_logbuffinfo0", loop, streamname);
+
+    WRITE_IMAGENAME(imnamebinfo1, "aol%ld_%s_logbuffinfo1", loop, streamname);
+
+    printf("DATA buffers:  %s  %s\n", imnameb0, imnameb1);
+    printf("INFO buffers:  %s  %s\n", imnamebinfo0, imnamebinfo1);
+
     IDinb0 = read_sharedmem_image(imnameb0);
     IDinb1 = read_sharedmem_image(imnameb1);
     IDinbinfo0 = read_sharedmem_image(imnamebinfo0);
     IDinbinfo1 = read_sharedmem_image(imnamebinfo1);
 
-	list_image_ID();
+    list_image_ID();
 
-/*    cnt0_old = data.image[IDinb0].md[0].cnt0;
-    cnt1_old = data.image[IDinb1].md[0].cnt0;
+    /*    cnt0_old = data.image[IDinb0].md[0].cnt0;
+        cnt1_old = data.image[IDinb1].md[0].cnt0;
 
-    xsize = data.image[IDinb0].md[0].size[0];
-    ysize = data.image[IDinb0].md[0].size[1];
-    xysize = xsize*ysize;
-    zsize = data.image[IDinb0].md[0].size[2];
-    atype = data.image[IDinb0].md[0].atype;
-  */ 
-  
+        xsize = data.image[IDinb0].md[0].size[0];
+        ysize = data.image[IDinb0].md[0].size[1];
+        xysize = xsize*ysize;
+        zsize = data.image[IDinb0].md[0].size[2];
+        atype = data.image[IDinb0].md[0].atype;
+      */
+
     return RETURN_SUCCESS;
 }
