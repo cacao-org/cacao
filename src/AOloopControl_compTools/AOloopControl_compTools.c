59c9fd74ab60d18efdbfdbecc22ec93b8c2f9f01
oguyon <oliv.guyon@gmail.com>
2020-04-16 09:50:05 -1000

    shorname added

--- src/AOloopControl_compTools/AOloopControl_compTools.c
+++ src/AOloopControl_compTools/AOloopControl_compTools.c
@@ -19,7 +19,7 @@
 // module default short name
 // all CLI calls to this module functions will be <shortname>.<funcname>
 // if set to "", then calls use <funcname>
-#define MODULE_SHORTNAME_DEFAULT ""
+#define MODULE_SHORTNAME_DEFAULT "cacaoct"
 
 // Module short description
 #define MODULE_DESCRIPTION       "AO loop control - computation tools"
