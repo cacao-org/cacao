6e96bd91bbf409c568e98ff57783d49545cffca4
oguyon <oliv.guyon@gmail.com>
2020-04-16 10:10:22 -1000

    shorname added

--- src/AOloopControl_computeCalib/AOloopControl_computeCalib.c
+++ src/AOloopControl_computeCalib/AOloopControl_computeCalib.c
@@ -12,7 +12,7 @@
 // module default short name
 // all CLI calls to this module functions will be <shortname>.<funcname>
 // if set to "", then calls use <funcname>
-#define MODULE_SHORTNAME_DEFAULT ""
+#define MODULE_SHORTNAME_DEFAULT "cacaocc"
 
 // Module short description
 #define MODULE_DESCRIPTION       "AO loop control compute calibration"
