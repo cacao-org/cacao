1e147317c49a0d283469c9be0b39fb76836b8097
oguyon <oliv.guyon@gmail.com>
2020-05-03 22:01:32 -1000

    fix types

--- src/AOloopControl_DM/AOloopControl_DM_config.c
+++ src/AOloopControl_DM/AOloopControl_DM_config.c
@@ -1,11 +1,11 @@
 /**
  * @file    AOloopControl_DM_config.c
  * @brief   DM control
- * 
+ *
  * To be used for AOloopControl module
  *
  *
- * 
+ *
  */
 
 #include <stdint.h>
@@ -76,9 +76,9 @@ errno_t AOloopControl_printDMconf()
     char voltname_str[13];
 
     printf("DM | on |  x |  y | Nbch | busy | ave | DClevel | monint  | stat | IDdisp | voltmode | volttype | stroke100 | IDvolt | maxvolt |   voltname  |\n");
-    for(DMindex=0; DMindex<NB_DMindex; DMindex++)
+    for(DMindex = 0; DMindex < NB_DMindex; DMindex++)
     {
-        if(dmdispcombconf[DMindex].voltmode==1)
+        if(dmdispcombconf[DMindex].voltmode == 1)
         {
             sprintf(IDvolt_str, "%3ld", dmdispcombconf[DMindex].IDvolt);
             sprintf(maxvolt_str, "%6.2f", dmdispcombconf[DMindex].MAXVOLT);
@@ -122,7 +122,7 @@ int AOloopControl_DM_dmdispcombstatus(long DMindex)
 {
     long long mcnt = 0;
     int ch;
-	//int wcol, wrow; // window size
+    //int wcol, wrow; // window size
 
 
     AOloopControl_DM_loadconf();
@@ -136,7 +136,7 @@ int AOloopControl_DM_dmdispcombstatus(long DMindex)
     init_pair(3, COLOR_GREEN, COLOR_BLACK);
     init_pair(4, COLOR_RED, COLOR_BLACK);
 
-    while( !kbdhit() )
+    while(!kbdhit())
     {
         usleep(dmdispcombconf[DMindex].moninterval);
         clear();
@@ -144,91 +144,138 @@ int AOloopControl_DM_dmdispcombstatus(long DMindex)
         print_header(" PRESS ANY KEY TO STOP MONITOR ", '-');
         attroff(A_BOLD);
         printw("monitor sample %ld\n", mcnt);
-        printw("\n"); 
+        printw("\n");
 
-        printw("=========== DM %d ==============================================\n", DMindex);
+        printw("=========== DM %d ==============================================\n",
+               DMindex);
         printw("\n");
         printw("ON                = %10d\n", dmdispcombconf[DMindex].ON);
-        printw("size              = %ld x %ld = %ld\n", dmdispcombconf[DMindex].xsize, dmdispcombconf[DMindex].ysize, dmdispcombconf[DMindex].xysize);
-        printw("NBchannel         = %10d      Number of DM channels\n", dmdispcombconf[DMindex].NBchannel);
+        printw("size              = %ld x %ld = %ld\n", dmdispcombconf[DMindex].xsize,
+               dmdispcombconf[DMindex].ysize, dmdispcombconf[DMindex].xysize);
+        printw("NBchannel         = %10d      Number of DM channels\n",
+               dmdispcombconf[DMindex].NBchannel);
         printw("\n");
 
-        printw("loopcnt           = %10ld     AO loop index\n", dmdispcombconf[DMindex].loopcnt);
-        printw("updatecnt         = %10ld     Number of DM updates\n", dmdispcombconf[DMindex].updatecnt);
+        printw("loopcnt           = %10ld     AO loop index\n",
+               dmdispcombconf[DMindex].loopcnt);
+        printw("updatecnt         = %10ld     Number of DM updates\n",
+               dmdispcombconf[DMindex].updatecnt);
         printw("busy              = %10d   \n", dmdispcombconf[DMindex].busy);
         printw("nsecwait          = %10ld ns\n", dmdispcombconf[DMindex].nsecwait);
-        printw("delay time        = %10.3f us\n", dmdispcombconf[DMindex].tdelay*1.0e6);
-        printw("disp->V time      = %10.3f us\n", dmdispcombconf[DMindex].time_disp2V*1.0e6);
+        printw("delay time        = %10.3f us\n",
+               dmdispcombconf[DMindex].tdelay * 1.0e6);
+        printw("disp->V time      = %10.3f us\n",
+               dmdispcombconf[DMindex].time_disp2V * 1.0e6);
 
-		
-		
-		if(dmdispcombconf[DMindex].TrigMode==1)
-			attron(A_BOLD);
+
+
+        if(dmdispcombconf[DMindex].TrigMode == 1)
+        {
+            attron(A_BOLD);
+        }
         printw("\n");
-        printw("=========== TRIGGER MODE = %d ====================================\n", dmdispcombconf[DMindex].TrigMode); 		
-		printw("TrigChan          = %10d      DM trigger channel\n", dmdispcombconf[DMindex].TrigChan);
-		printw("TrigSem           = %10d      DM trigger semaphore\n", dmdispcombconf[DMindex].TrigSem);
-		printw("================================================================\n");
-		if(dmdispcombconf[DMindex].TrigMode==1)
-			attroff(A_BOLD);
-        printw("\n");     
+        printw("=========== TRIGGER MODE = %d ====================================\n",
+               dmdispcombconf[DMindex].TrigMode);
+        printw("TrigChan          = %10d      DM trigger channel\n",
+               dmdispcombconf[DMindex].TrigChan);
+        printw("TrigSem           = %10d      DM trigger semaphore\n",
+               dmdispcombconf[DMindex].TrigSem);
+        printw("================================================================\n");
+        if(dmdispcombconf[DMindex].TrigMode == 1)
+        {
+            attroff(A_BOLD);
+        }
+        printw("\n");
 
 
 
-		if(dmdispcombconf[DMindex].voltmode==1)
-			attron(A_BOLD);			
-		printw("=========== OUTPUT VOLT ========================================\n");
-        printw("voltmode          = %10d      Configured for output voltage ?\n", dmdispcombconf[DMindex].voltmode);
-        printw("voltON            = %10d      DM voltage ouptut activated ?\n", dmdispcombconf[DMindex].voltON);
-        printw("volttype          = %10d      DM type (1:linear bipolar, 2: quadratic unipolar)\n", dmdispcombconf[DMindex].volttype);
-        printw("stroke100         = %10.3f um   Displacement [um] for 100 V\n",             dmdispcombconf[DMindex].stroke100);
-        printw("MAXVOLT           = %10.2f V    Maximum voltage\n",             dmdispcombconf[DMindex].MAXVOLT);
-        printw("AveMode           = %10d      Averaging mode for combined displacement\n", dmdispcombconf[DMindex].AveMode);
+        if(dmdispcombconf[DMindex].voltmode == 1)
+        {
+            attron(A_BOLD);
+        }
+        printw("=========== OUTPUT VOLT ========================================\n");
+        printw("voltmode          = %10d      Configured for output voltage ?\n",
+               dmdispcombconf[DMindex].voltmode);
+        printw("voltON            = %10d      DM voltage ouptut activated ?\n",
+               dmdispcombconf[DMindex].voltON);
+        printw("volttype          = %10d      DM type (1:linear bipolar, 2: quadratic unipolar)\n",
+               dmdispcombconf[DMindex].volttype);
+        printw("stroke100         = %10.3f um   Displacement [um] for 100 V\n",
+               dmdispcombconf[DMindex].stroke100);
+        printw("MAXVOLT           = %10.2f V    Maximum voltage\n",
+               dmdispcombconf[DMindex].MAXVOLT);
+        printw("AveMode           = %10d      Averaging mode for combined displacement\n",
+               dmdispcombconf[DMindex].AveMode);
         printw("    0: Offset combined to DC level\n");
         printw("       Clip displacement at 0.0\n");
         printw("    1: Remove average and apply DC offset.\n");
         printw("       Clip at 0.0.\n");
         printw("    2: Do not apply DC offset, do not offset sum, do not clip\n");
-        printw("DClevel           = %10.5f um   Displacement DC offset\n", dmdispcombconf[DMindex].DClevel);
-		printw("================================================================\n");
-		if(dmdispcombconf[DMindex].voltmode==1)
-			attroff(A_BOLD);
-        printw("\n"); 
-
-        for(ch=0; ch<dmdispcombconf[DMindex].NBchannel; ch++)
-            printw(" CHANNEL %2d  gain = %10.3f   dm%02lddisp%02ld   %10ld\n", ch, dmdispcombconf[DMindex].dmdispgain[ch], DMindex, ch, dmdispcombconf[DMindex].dmdispcnt[ch]);
-        printw("\n"); 
-
-		if(dmdispcombconf[DMindex].dm2dm_mode==1)
-			attron(A_BOLD);
-		printw("=========== DM-to-DM OUPUT (CPU-based) ==========================\n");
-        printw("dm2dm_mode        = %10d      DM controls an output DM ?\n", dmdispcombconf[DMindex].dm2dm_mode);
-        printw("xsizeout          = %10ld      x size of output DM\n", dmdispcombconf[DMindex].xsizeout);
-        printw("ysizeout          = %10ld      y size of output DM\n", dmdispcombconf[DMindex].ysizeout);
-        printw("dm2dm_DMmodes     = %10s      output DM modes\n", dmdispcombconf[DMindex].dm2dm_DMmodes_name);
-        printw("dm2dm_outdisp     = %10s      ouput DM displacement\n", dmdispcombconf[DMindex].dm2dm_outdisp_name);
+        printw("DClevel           = %10.5f um   Displacement DC offset\n",
+               dmdispcombconf[DMindex].DClevel);
         printw("================================================================\n");
-		if(dmdispcombconf[DMindex].dm2dm_mode==1)
-			attroff(A_BOLD);
+        if(dmdispcombconf[DMindex].voltmode == 1)
+        {
+            attroff(A_BOLD);
+        }
         printw("\n");
 
-		if(dmdispcombconf[DMindex].dm2dm_mode==1)
-			attron(A_BOLD);
-		printw("======== DM CONTROL TO OUTPUT WFS REFERENCE (CPU-based) ========\n");
-		printw("wfsrefmode        = %10d      DM controls output wfsref ?\n", dmdispcombconf[DMindex].wfsrefmode);
-        printw("xsizewfsref       = %10ld      x size of output WFS ref\n", dmdispcombconf[DMindex].xsizeout);
-        printw("ysizewfsref       = %10ld      y size of output WFS ref\n", dmdispcombconf[DMindex].ysizeout);
-		printw("wfsref_RespMat    = %10s      DM-to-WFSref matrix\n", dmdispcombconf[DMindex].wfsref_RespMat_name);
-		printw("wfsref_out        = %10s      output WFSref\n", dmdispcombconf[DMindex].wfsref_out_name);
-        printw("================================================================\n");
-		if(dmdispcombconf[DMindex].dm2dm_mode==1)
-			attroff(A_BOLD);
+        for(ch = 0; ch < dmdispcombconf[DMindex].NBchannel; ch++)
+        {
+            printw(" CHANNEL %2d  gain = %10.3f   dm%02lddisp%02ld   %10ld\n", ch,
+                   dmdispcombconf[DMindex].dmdispgain[ch], DMindex, ch,
+                   dmdispcombconf[DMindex].dmdispcnt[ch]);
+        }
         printw("\n");
-		
+
+        if(dmdispcombconf[DMindex].dm2dm_mode == 1)
+        {
+            attron(A_BOLD);
+        }
+        printw("=========== DM-to-DM OUPUT (CPU-based) ==========================\n");
+        printw("dm2dm_mode        = %10d      DM controls an output DM ?\n",
+               dmdispcombconf[DMindex].dm2dm_mode);
+        printw("xsizeout          = %10ld      x size of output DM\n",
+               dmdispcombconf[DMindex].xsizeout);
+        printw("ysizeout          = %10ld      y size of output DM\n",
+               dmdispcombconf[DMindex].ysizeout);
+        printw("dm2dm_DMmodes     = %10s      output DM modes\n",
+               dmdispcombconf[DMindex].dm2dm_DMmodes_name);
+        printw("dm2dm_outdisp     = %10s      ouput DM displacement\n",
+               dmdispcombconf[DMindex].dm2dm_outdisp_name);
+        printw("================================================================\n");
+        if(dmdispcombconf[DMindex].dm2dm_mode == 1)
+        {
+            attroff(A_BOLD);
+        }
+        printw("\n");
+
+        if(dmdispcombconf[DMindex].dm2dm_mode == 1)
+        {
+            attron(A_BOLD);
+        }
+        printw("======== DM CONTROL TO OUTPUT WFS REFERENCE (CPU-based) ========\n");
+        printw("wfsrefmode        = %10d      DM controls output wfsref ?\n",
+               dmdispcombconf[DMindex].wfsrefmode);
+        printw("xsizewfsref       = %10ld      x size of output WFS ref\n",
+               dmdispcombconf[DMindex].xsizeout);
+        printw("ysizewfsref       = %10ld      y size of output WFS ref\n",
+               dmdispcombconf[DMindex].ysizeout);
+        printw("wfsref_RespMat    = %10s      DM-to-WFSref matrix\n",
+               dmdispcombconf[DMindex].wfsref_RespMat_name);
+        printw("wfsref_out        = %10s      output WFSref\n",
+               dmdispcombconf[DMindex].wfsref_out_name);
+        printw("================================================================\n");
+        if(dmdispcombconf[DMindex].dm2dm_mode == 1)
+        {
+            attroff(A_BOLD);
+        }
+        printw("\n");
+
         printw("status            = %10d\n",  dmdispcombconf[DMindex].status);
         printw("moninterval       = %10d us\n", dmdispcombconf[DMindex].moninterval);
         printw("\n");
-     
+
         mcnt++;
         refresh();
     }
@@ -243,11 +290,13 @@ int AOloopControl_DM_dmdispcombstatus(long DMindex)
 
 int AOloopControl_DM_chan_setgain(long DMindex, int ch, float gain)
 {
-    
+
     AOloopControl_DM_loadconf();
- 
-    if(ch<dmdispcombconf[DMindex].NBchannel)
+
+    if(ch < dmdispcombconf[DMindex].NBchannel)
+    {
         dmdispcombconf[DMindex].dmdispgain[ch] = gain;
+    }
 
     return 0;
 }
@@ -259,8 +308,8 @@ int AOloopControl_DM_setvoltON(long DMindex)
 {
     AOloopControl_DM_loadconf();
     dmdispcombconf[DMindex].voltON = 1;
-	AOloopControl_printDMconf();
-	
+    AOloopControl_printDMconf();
+
     return 0;
 }
 
@@ -269,8 +318,8 @@ int AOloopControl_DM_setvoltOFF(long DMindex)
 {
     AOloopControl_DM_loadconf();
     dmdispcombconf[DMindex].voltON = 0;
-	AOloopControl_printDMconf();
-	
+    AOloopControl_printDMconf();
+
     return 0;
 }
 
@@ -279,8 +328,8 @@ int AOloopControl_DM_setMAXVOLT(long DMindex, float maxvolt)
 {
     AOloopControl_DM_loadconf();
     dmdispcombconf[DMindex].MAXVOLT = maxvolt;
-	AOloopControl_printDMconf();
-	
+    AOloopControl_printDMconf();
+
     return 0;
 }
 
@@ -289,7 +338,7 @@ int AOloopControl_DM_setDClevel(long DMindex, float DClevel)
 {
     AOloopControl_DM_loadconf();
     dmdispcombconf[DMindex].DClevel = DClevel;
-	AOloopControl_printDMconf();
+    AOloopControl_printDMconf();
 
     return 0;
 }
@@ -299,7 +348,7 @@ int AOloopControl_DM_setAveMode(long DMindex, int AveMode)
 {
     AOloopControl_DM_loadconf();
     dmdispcombconf[DMindex].AveMode = AveMode;
-	AOloopControl_printDMconf();
+    AOloopControl_printDMconf();
 
     return 0;
 }
@@ -309,7 +358,7 @@ int AOloopControl_DM_setTrigMode(long DMindex, int mode)
 {
     AOloopControl_DM_loadconf();
     dmdispcombconf[DMindex].TrigMode = mode;
-	AOloopControl_printDMconf();
+    AOloopControl_printDMconf();
 
     return 0;
 }
@@ -318,7 +367,7 @@ int AOloopControl_DM_setTrigChan(long DMindex, int chan)
 {
     AOloopControl_DM_loadconf();
     dmdispcombconf[DMindex].TrigChan = chan;
-	AOloopControl_printDMconf();
+    AOloopControl_printDMconf();
 
     return 0;
 }
@@ -327,7 +376,7 @@ int AOloopControl_DM_setTrigSem(long DMindex, int sem)
 {
     AOloopControl_DM_loadconf();
     dmdispcombconf[DMindex].TrigSem = sem;
-	AOloopControl_printDMconf();
+    AOloopControl_printDMconf();
 
     return 0;
 }
