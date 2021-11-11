6d085fd763c05d0688f7c4e7dba86e03efcdf03f
Olivier Guyon <oliv.guyon@gmail.com>
2021-11-11 01:16:19 -1000

    clip aveval

--- AOloopControl/modalfilter.c
+++ AOloopControl/modalfilter.c
@@ -200,6 +200,7 @@ static errno_t compute_function()
 
         avemval[mi] = (1.0-avegain)*avemval[mi] + avegain * (imgin.im->array.F[mi] * gain);
 
+
         // update long term average if input mode values
         mvalout[mi] = (1.0-gain)*mvalout[mi] - gain * (imgin.im->array.F[mi] - (*aftgain)*avemval[mi] );
         mvalout[mi] *= mult;
@@ -212,6 +213,16 @@ static errno_t compute_function()
         if(mvalout[mi] < -limitval)
         {
             mvalout[mi] = -limitval;
+        }
+
+        if(avemval[mi] > limitval)
+        {
+            avemval[mi] = limitval;
+        }
+
+        if(avemval[mi] < -limitval)
+        {
+            avemval[mi] = -limitval;
         }
     }
 
