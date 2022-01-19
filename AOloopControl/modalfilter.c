e2bbb56a24a7dae73d01b0384631d5b4dedbe74e
Olivier Guyon <oliv.guyon@gmail.com>
2022-01-19 13:02:15 -1000

    test

--- AOloopControl/modalfilter.c
+++ AOloopControl/modalfilter.c
@@ -185,7 +185,7 @@ static errno_t compute_function()
             avemval[mi] = (1.0 - avegain) * avemval[mi] + avegain * (imgin.im->array.F[mi] * gain);
 
             // update long term average if input mode values
-            mvalout[mi] = gain * (imgin.im->array.F[mi]);
+            mvalout[mi] = (1.0 - gain) * mvalout[mi] - gain * (imgin.im->array.F[mi]);
             //(1.0 - gain) * mvalout[mi] - gain * (imgin.im->array.F[mi]);
             //            - (*aftgain) * avemval[mi]);
             //mvalout[mi] *= mult;
