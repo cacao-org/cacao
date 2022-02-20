c397a426b848d2b4222093c36cb606eb5064eac3
Olivier Guyon <oliv.guyon@gmail.com>
2022-02-20 03:04:07 -1000

    verbose

--- AOloopControl/modalCTRL_stats.c
+++ AOloopControl/modalCTRL_stats.c
@@ -309,7 +309,8 @@ static errno_t compute_function()
                 //block_WFSrms2[block] /= block_cnt[block];
                 //block_OLrms2[block] /= block_cnt[block];
                 printf(
-                    "BLOCK %2d (%5ld modes)   WFS = %7.3f (%7.3f %7.3f)  DM = "
+                    "BLOCK %2d (%5ld modes) RMS  WFS = %7.3f (noise = %7.3f "
+                    "%7.3f)  DM = "
                     "%7.3f   "
                     "OL = "
                     "%7.3f   [nm]  --> %5.3f\n",
@@ -320,10 +321,11 @@ static errno_t compute_function()
                     1000.0 * sqrt(block_WFSmqrms2[block]),
                     1000.0 * sqrt(block_DMrms2[block]),
                     1000.0 * sqrt(block_OLrms2[block]),
-                    sqrt(block_OLrms2[block]) / sqrt(block_WFSrms2[block]));
+                    sqrt(block_WFSrms2[block]) / sqrt(block_OLrms2[block]));
             }
         }
     }
+
 
 
     INSERT_STD_PROCINFO_COMPUTEFUNC_END
