3c44025317c1ca3019aecfc18cceb641b628dd96
Olivier Guyon <oliv.guyon@gmail.com>
2022-11-20 10:05:21 -1000

    enable default LOOPROOTDIR

--- AOloopControl/examples/KalAO-dmloop-conf/cacaovars.dmloop.bash
+++ AOloopControl/examples/KalAO-dmloop-conf/cacaovars.dmloop.bash
@@ -1,20 +1,38 @@
 # cacao environment variables for setup
 # This file will be sourced by cacao-setup
 
+
+export CACAO_LOOPNUMBER="1"
+
+
+
+# ====== DEFORMABLE MIRROR ==========
+
+# Deformable mirror (DM) size
+# If DM is single dimension, enter "1" for DMsize
+#
+export CACAO_DMINDEX="01"
+export CACAO_DMxsize="12"
+export CACAO_DMysize="12"
+
 # 1 if DM actuators are on a coordinate grid
 # This informs processes if a spatial relationship exists
 # between DM actuators
 export CACAO_DMSPATIAL="1"
 
-# Deformable mirror (DM) size
-# If DM is single dimension, enter "1" for DMsize
-#
-export CACAO_DMxsize="12"
-export CACAO_DMysize="12"
 
+
+# ====== DIRECTORIES ================
+
+# Optional. If not defined, will take value "LOOPNAME-rootdir"
 export CACAO_LOOPROOTDIR="dmloop"
-export CACAO_DMINDEX="01"
-export CACAO_LOOPNUMBER="1"
+
+# Optional. If not defined, will take value "LOOPNAME-rundir"
+# export CACAO_LOOPRUNDIR="dmloop"
+
+
+
+
 
 # input WFS stream
 export CACAO_WFSSTREAM="shwfs_slopes"
