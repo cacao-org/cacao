d41162036e7492ef7da9d317d57cff644c890483
oguyon <oliv.guyon@gmail.com>
2023-03-03 19:26:46 -1000

    update calib files

--- AOloopControl/examples/scexao-vispyr-bin2-conf/simLHS/respM.fits.get.bash
+++ AOloopControl/examples/scexao-vispyr-bin2-conf/simLHS/respM.fits.get.bash
@@ -4,12 +4,12 @@ FILENAME="respM.fits"
 
 if [ ! -f $FILENAME ]; then
 
-    ggID='1D1SfU2gZpzEU-tRZK0bJsL6MUYZ09Ils'
+    ggID='1MnNo9WfRHuKik3gpEHEjZHgt8_PM0T1a'
     ggURL='https://drive.google.com/uc?export=download'
     filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
     getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
     curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"
-    mv LHS_zrespM_2018-01-25_05:09:52.fits ${FILENAME}
+    mv scexao-vispyr-bin2-zrespM.fits ${FILENAME}
 
 fi
 
