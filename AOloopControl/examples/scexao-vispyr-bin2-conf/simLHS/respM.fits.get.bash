66186d2a5d5380aa573cf22dd30f9eda5f159c67
jared <jaredmales@gmail.com>
2023-08-24 21:35:39 +0000

    handle case where curl doesn't create cookie file when empty

--- AOloopControl/examples/scexao-vispyr-bin2-conf/simLHS/respM.fits.get.bash
+++ AOloopControl/examples/scexao-vispyr-bin2-conf/simLHS/respM.fits.get.bash
@@ -6,9 +6,9 @@ if [ ! -f $FILENAME ]; then
 
     ggID='1MnNo9WfRHuKik3gpEHEjZHgt8_PM0T1a'
     ggURL='https://drive.google.com/uc?export=download'
-    filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
-    getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
-    curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"
+    filename="$(curl -sc /tmp/gcookie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+    getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcookie)" || touch /tmp/gcookie
+    curl -Lb /tmp/gcookie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"
     mv scexao-vispyr-bin2-zrespM.fits ${FILENAME}
 
 fi
