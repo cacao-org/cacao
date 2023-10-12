a9af7c1cae922aaaafbc091c512a28d5a1122bda
Jonathan Lin <jon880@astro.ucla.edu>
2023-10-11 21:00:05 -1000

    test

--- AOloopControl_IOtools/pl_trace_renorm.c
+++ AOloopControl_IOtools/pl_trace_renorm.c
@@ -1,6 +1,6 @@
 /**
- * @file    .c
- * @brief
+ * @file    pl_trace_renorm.c
+ * @brief   acquire and normalize spectra of 3-port PL
  *
  *
  *
@@ -9,13 +9,15 @@
 
 #include "CommandLineInterface/CLIcore.h"
 
-static long NUM_APD_HOWFS = 188;
-static long NUM_APD_LOWFS = 16;
-
 // Local variables pointers
-static char *input_shm_name; // THE FPS PARAMETER VARIABLE
+static char *input_shm_name; // input shared memory
 static long  fpi_inputshmname;
 
+static char *output_shm_name; // output shared memory
+static long  fpi_outputshmname;
+
+static int64_t *compWFSnormalize; // spec norm toggle (lifted this from acquireWFSim.c heh)
+static long     fpi_compWFSnormalize;
 
 static CLICMDARGDEF farg[] =
 {
@@ -28,7 +30,24 @@ static CLICMDARGDEF farg[] =
         (void **) &input_shm_name,
         &fpi_inputshmname
     },
-    // TODO time-downsample factor for LOWFS.
+    {
+        CLIARG_STR,
+        ".wfsout",
+        "Wavefront sensor output",
+        "wfsim",
+        CLIARG_VISIBLE_DEFAULT,
+        (void **) &output_shm_name,
+        &fpi_outputshmname
+    }
+    {
+        CLIARG_ONOFF,
+        ".comp.WFSnormalize",
+        "normalize WFS frames -> imWFS1",
+        "1",
+        CLIARG_HIDDEN_DEFAULT,
+        (void **) &compWFSnormalize,
+        &fpi_compWFSnormalize
+    },
 };
 
 // Optional custom configuration setup.
@@ -58,21 +77,48 @@ static errno_t help_function()
     return RETURN_SUCCESS;
 }
 
-
 static errno_t compute_function()
 {
     DEBUG_TRACE_FSTART();
-
     // JON YOU GET TO INIT HERE
+
+    IMGID wfsin = mkIMGID_from_name(input_shm_name);
+    resolveIMGID(&wfsin, ERRMODE_ABORT);
+
+    uint32_t sizeoutx = wfsin.size[0];
+    uint32_t sizeouty = wfsin.size[1];
+    uint32_t sizeout = sizeoutx * sizeouty;
+
+    // Create output
+    // trying to copy shape of wfsin. Assume a structure 3 rows x N columns, N goes along wavelength
+    IMGID wfsout;
+    wfsout =
+        stream_connect_create_2D(output_shm_name, sizeoutx, sizeouty, _DATATYPE_FLOAT);
 
     // This is the while(True) {
     INSERT_STD_PROCINFO_COMPUTEFUNC_INIT
     INSERT_STD_PROCINFO_COMPUTEFUNC_LOOPSTART
     {
         // JON YOU GET TO WORK HERE
+        if(data.fpsptr->parray[fpi_compWFSnormalize].fpflag & FPFLAG_ONOFF) {
+            for (j = 0; j < sizeouty; j++){
+                double tot = 0.0;
+                for (i = 0; i < sizeoutx; i++) {
+                    tot += wfsin.im->array[i][j];
+                }
+                for (i = 0; i < sizeoutx; i++) {
+                    wfsout.im->array[i][j] /= tot;
+                }
+            }
+        }
+        else {
+            memcpy(wfsout.im->array.F,
+            wfsin.im->array.F,
+            sizeof(float) * sizeout);
+        }
 
         // Done and post downstream.
-        processinfo_update_output_stream(processinfo, curv_2k_singlesided.ID);
+        processinfo_update_output_stream(processinfo, wfsout.ID);
     }
     INSERT_STD_PROCINFO_COMPUTEFUNC_END // } // while (true)
 
@@ -80,8 +126,6 @@ static errno_t compute_function()
 
     return RETURN_SUCCESS;
 }
-
-
 
 
 INSERT_STD_FPSCLIfunctions
