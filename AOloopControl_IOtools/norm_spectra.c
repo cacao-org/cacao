484ef9c6557135f325135687809ec7c5c969b7d8
Jonathan Lin <jon880@astro.ucla.edu>
2023-10-19 09:39:25 -1000

    fixy

--- AOloopControl_IOtools/norm_spectra.c
+++ AOloopControl_IOtools/norm_spectra.c
@@ -128,7 +128,7 @@ static errno_t compute_function()
         else {
             memcpy(wfsout.im->array.F,
             wfsin.im->array.F,
-            sizeof(float) * sizeout);
+            sizeof(float) * sizeoutx * sizouty);
         }
         
         // Done and post downstream.
