de00dd256e0944884b494b35d673cc92521e2b91
Jonathan Lin <90280749+jw-lin@users.noreply.github.com>
2023-11-09 12:16:36 -0800

    Update wfsref.fits.get.bash
    changing wfsref to download 3PL spectro version

--- AOloopControl/examples/scexao-NIRPL-conf/simLHS/wfsref.fits.get.bash
+++ AOloopControl/examples/scexao-NIRPL-conf/simLHS/wfsref.fits.get.bash
@@ -4,7 +4,7 @@ FILENAME="wfsref.fits"
 
 if [ ! -f ${FILENAME} ]; then
     echo "Downloading $FILENAME"
-    wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1wg9is7ZGZao2OZsgktmGTJgvFi4_XKAE' -O ${FILENAME}.gz
+    wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1JxfPrckTb-rPysNHKtHFkr9g0dv7IfN4' -O ${FILENAME}.gz
     gunzip ${FILENAME}.gz
 fi
 
