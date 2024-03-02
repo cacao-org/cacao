56122325251f97c7786564542b892dd6fe04b7bb
oguyon <oliv.guyon@gmail.com>
2024-03-01 19:24:24 -1000

    moved calibs in example

--- AOloopControl/examples/scexao-vispyr-bin2-conf/simLHS/wfsref.fits.get.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-FILENAME="wfsref.fits"
-
-if [ ! -f ${FILENAME} ]; then
-    echo "Downloading $FILENAME"
-    wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=15V2iMIeflC3gCdic9hTSNOpiuFAAKifa' -O ${FILENAME}
-fi
