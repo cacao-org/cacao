be77f9c890c4505684053eb29a2007cc2811e152
oguyon <oliv.guyon@gmail.com>
2024-03-03 12:40:34 -1000

    use compressed calibs

--- AOloopControl/examples/scexao-NIRPL-conf/simLHS/respM.fits.get.bash
+++ AOloopControl/examples/scexao-NIRPL-conf/simLHS/respM.fits.get.bash
@@ -3,12 +3,7 @@
 FILENAME="respM.fits"
 
 if [ ! -f $FILENAME ]; then
-
-    ggID='1XEAKFsXBH6ppJhKbZdUlCygO1YfVLd1O'
-    ggURL='https://drive.google.com/uc?export=download'
-    filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
-    getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
-    curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"
-    gunzip scexao-NIRPL-zrespM.fits.gz
+	# upack compressed file
+	funpack scexao-NIRPL-zrespM.fits.fz
     mv scexao-NIRPL-zrespM.fits ${FILENAME}
 fi
--- AOloopControl/examples/scexao-NIRPL-conf/simLHS/wfsref.fits.get.bash
+++ AOloopControl/examples/scexao-NIRPL-conf/simLHS/wfsref.fits.get.bash
@@ -3,8 +3,8 @@
 FILENAME="wfsref.fits"
 
 if [ ! -f ${FILENAME} ]; then
-    echo "Downloading $FILENAME"
-    wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1wg9is7ZGZao2OZsgktmGTJgvFi4_XKAE' -O ${FILENAME}.gz
-    gunzip ${FILENAME}.gz
+    # uncompressing file
+    funpack scexao-NIRPL-wfsref.fits.fz
+    mv scexao-NIRPL-wfsref.fits ${FILENAME}
 fi
 
