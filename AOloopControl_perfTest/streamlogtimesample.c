26696dcc3c575d8d116d02390afbfa54cadefb8a
oguyon <oliv.guyon@gmail.com>
2025-07-09 01:48:15 -1000

    activate write log

--- AOloopControl_perfTest/streamlogtimesample.c
+++ AOloopControl_perfTest/streamlogtimesample.c
@@ -954,11 +954,11 @@ static errno_t compute_function()
                                    datfile[idatfile].name);
                     if(*lintiming[sindex] == 1)
                     {
-                        processTimingFile(fnameTXT, fnameTXTout, NULL, PROCESSTIMINGFLAG_LOAD|PROCESSTIMINGFLAG_LINTIMING, tarrayin);
+                        processTimingFile(fnameTXT, fnameTXTout, NULL, PROCESSTIMINGFLAG_LOAD|PROCESSTIMINGFLAG_LINTIMING|PROCESSTIMINGFLAG_WRITE, tarrayin);
                     }
                     else
                     {
-                        processTimingFile(fnameTXT, fnameTXTout, NULL, PROCESSTIMINGFLAG_LOAD, tarrayin);
+                        processTimingFile(fnameTXT, fnameTXTout, NULL, PROCESSTIMINGFLAG_LOAD|PROCESSTIMINGFLAG_WRITE, tarrayin);
                     }
                 }
 
