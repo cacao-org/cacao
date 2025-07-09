797c85301e7a50ae70fb46e014b4790b708200d4
oguyon <oliv.guyon@gmail.com>
2025-07-09 03:38:16 -1000

    floatipt notation

--- AOloopControl_perfTest/wfsrefoptimselect.c
+++ AOloopControl_perfTest/wfsrefoptimselect.c
@@ -249,7 +249,7 @@ static errno_t WFSref_optimizeWFS_PSFselect(
     for(uint32_t frame=0; frame < zsize; frame++)
     {
         long slice = imindex[zsize-frame-1];
-        printf("frame %5d  slice %5ld   val %11.9f\n", frame, slice, psfvalue[frame]);
+        printf("frame %5d  slice %5ld   val %g\n", frame, slice, psfvalue[frame]);
 
         char *ptr0 = (char*) psfimg.im->array.F;
         ptr0 += sizeof(float)*psfxysize*slice;
