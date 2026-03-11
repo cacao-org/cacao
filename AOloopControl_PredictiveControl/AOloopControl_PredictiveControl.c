9c896a6bd1f0f92fc9a702cf2584a9ff70b28f4d
oguyon <oliv.guyon@gmail.com>
2026-03-11 09:54:25 -1000

    Migrate CLI registrations to Gen 4 V2
    Convert AOloopControl_PredictiveControl.c: 4 commands
    (aolPFwatchin, aolmappfilt, aolmkpfilt, aolpfsetave).
    Convert computeCalib.c: 1 command (aolHaddec).
    
    Build: 0 errors. Tests: 35/36 pass.

--- AOloopControl_PredictiveControl/AOloopControl_PredictiveControl.c
+++ AOloopControl_PredictiveControl/AOloopControl_PredictiveControl.c
@@ -1,221 +1,413 @@
 /**
  * @file    AOloopControl_PredictiveControl.c
- * @brief   Adaptive Optics Control loop engine Predictive Control
+ * @brief   AO predictive control
  *
- * Adaptive Optics predictive control
- *
- *
- *
+ * Uses FPS V2 framework.
  */
 
-/* ================================================================== */
-/* ================================================================== */
-/*            MODULE INFO                                             */
-/* ================================================================== */
-/* ================================================================== */
-
-// module default short name
-// all CLI calls to this module functions will be <shortname>.<funcname>
-// if set to "", then calls use <funcname>
 #define MODULE_SHORTNAME_DEFAULT "cacaopc"
-
-// Module short description
-#define MODULE_DESCRIPTION "AO loop control predictive control"
-
-// Application to which module belongs
+#define MODULE_DESCRIPTION \
+    "AO loop control predictive control"
 #define MODULE_APPLICATION "cacao-cli"
 
 #define _GNU_SOURCE
 
-/* ===============================================================================================
- */
-/* ===============================================================================================
- */
-/*                                        HEADER FILES */
-/* ===============================================================================================
- */
-/* ===============================================================================================
- */
-
 #include "CLIcore/CLIcore.h"
+#include "fps.h"
 
 #include "AOloopControl/AOloopControl.h"
 #include "AOloopControl_PredictiveControl/AOloopControl_PredictiveControl.h"
 
-/* ================================================================== */
-/* ================================================================== */
-/*            INITIALIZE LIBRARY                                      */
-/* ================================================================== */
-/* ================================================================== */
-
-// Module initialization macro in CLIcore.h
-// macro argument defines module name for bindings
-//
 INIT_MODULE_LIB(AOloopControl_PredictiveControl)
 
-/* ================================================================== */
-/* ================================================================== */
-/*            COMMAND LINE INTERFACE (CLI) FUNCTIONS                  */
-/* ================================================================== */
-/* ================================================================== */
 
-/* ===============================================================================================
- */
-/* ===============================================================================================
- */
-/** @name AOloopControl_PredictiveControl - 1. PREDICTIVE CONTROL
- *  Predictive control using WFS telemetry */
-/* ===============================================================================================
- */
-/* ===============================================================================================
- */
+/* =============================================================
+ *  Parameters
+ * ============================================================ */
 
-/** @brief CLI function for AOloopControl_builPFloop_WatchInput */
-errno_t AOloopControl_PredictiveControl_builPFloop_WatchInput_cli()
+static long long p_loop     = 0;
+static long long p_pfblock  = 2;
+static long long p_pfstart  = 0;
+static long long p_pfend    = 0;
+static long long p_nbbuff   = 0;
+static char p_modecoeff[
+    FUNCTION_PARAMETER_STRMAXLEN]
+    = "coeffim";
+static long long p_modeout  = 23;
+static double    p_delayfr  = 2.4;
+static long long p_filtsize = 20;
+static char p_filtname[
+    FUNCTION_PARAMETER_STRMAXLEN]
+    = "filt23";
+static char p_pfname[
+    FUNCTION_PARAMETER_STRMAXLEN]
+    = "outPFb0";
+static double    p_decaycoeff = 0.5;
+
+
+/* =============================================================
+ *  CMD 1: aolPFwatchin (5 args, primary)
+ * ============================================================ */
+
+static FPS_APP_INFO FPS_app_info = {
+    .fps_name    = "aolPFwatchin",
+    .cmdkey      = "aolPFwatchin",
+    .description =
+        "watch telemetry for PF input"
+};
+
+#define FPS_PARAMS(X) \
+    X(".loop", &p_loop, \
+      FPTYPE_INT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "loop number") \
+    X(".pfblock", &p_pfblock, \
+      FPTYPE_INT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "PF block number") \
+    X(".pfstart", &p_pfstart, \
+      FPTYPE_INT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "PF block start") \
+    X(".pfend", &p_pfend, \
+      FPTYPE_INT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "PF block end") \
+    X(".nbbuff", &p_nbbuff, \
+      FPTYPE_INT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "number of buffers")
+
+static FPS_CLI_BINDING my_bindings[] = {
+    FPS_PARAMS(FPS_X_BINDING)
+};
+static const int nb_bindings =
+    sizeof(my_bindings) /
+    sizeof(FPS_CLI_BINDING);
+
+static CLICMDARGDEF farg[] = {
+    FPS_PARAMS(FPS_X_FARG)
+};
+
+static CLICMDDATA CLIcmddata = {
+    "", "", CLICMD_FIELDS_DEFAULTS
+};
+static CMDSETTINGS cms = {0};
+
+static __attribute__((constructor))
+void init_cms(void)
 {
-    if(CLI_checkarg(1, 2) + CLI_checkarg(2, 2) + CLI_checkarg(3, 2) +
-            CLI_checkarg(4, 2) + CLI_checkarg(5, 2) ==
-            0)
-    {
-        AOloopControl_PredictiveControl_builPFloop_WatchInput(
-            data.cmdargtoken[1].val.numl,
-            data.cmdargtoken[2].val.numl,
-            data.cmdargtoken[3].val.numl,
-            data.cmdargtoken[4].val.numl,
-            data.cmdargtoken[5].val.numl);
-
-        return CLICMD_SUCCESS;
-    }
-    else
-    {
-        return CLICMD_INVALID_ARG;
+    strncpy(CLIcmddata.key,
+            FPS_app_info.cmdkey,
+            sizeof(CLIcmddata.key) - 1);
+    strncpy(CLIcmddata.description,
+            FPS_app_info.description,
+            sizeof(CLIcmddata.description)
+            - 1);
+    if (CLIcmddata.cmdsettings == NULL) {
+        CLIcmddata.cmdsettings = &cms;
     }
 }
 
-/** @brief CLI function for AOloopControl_mapPredictiveFilter */
-errno_t AOloopControl_PredictiveControl_mapPredictiveFilter_cli()
+static errno_t compute_function()
 {
-    if(CLI_checkarg(1, 4) + CLI_checkarg(2, 2) + CLI_checkarg(3, 1) == 0)
-    {
-        AOloopControl_PredictiveControl_mapPredictiveFilter(
-            data.cmdargtoken[1].val.string,
-            data.cmdargtoken[2].val.numl,
-            data.cmdargtoken[3].val.numf);
+    AOloopControl_PredictiveControl_builPFloop_WatchInput(
+        p_loop, p_pfblock,
+        p_pfstart, p_pfend,
+        p_nbbuff);
+    return RETURN_SUCCESS;
+}
 
-        return CLICMD_SUCCESS;
-    }
-    else
-    {
-        return CLICMD_INVALID_ARG;
+
+/* =============================================================
+ *  CMD 2: aolmappfilt (3 args)
+ * ============================================================ */
+
+static FPS_APP_INFO FPS_app_info_map = {
+    .fps_name    = "aolmappfilt",
+    .cmdkey      = "aolmappfilt",
+    .description =
+        "map/search predictive filter"
+};
+
+#define FPS_PARAMS_MAP(X) \
+    X(".modecoeff", p_modecoeff, \
+      FPTYPE_STREAMNAME, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "input coefficients") \
+    X(".modeout", &p_modeout, \
+      FPTYPE_INT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "mode number") \
+    X(".delayfr", &p_delayfr, \
+      FPTYPE_FLOAT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "delay in frames")
+
+static CLICMDDATA CLIcmddata_map = {
+    "", "", CLICMD_FIELDS_NOPARAM
+};
+static CMDSETTINGS cms_map = {0};
+
+static __attribute__((constructor))
+void init_cms_map(void)
+{
+    strncpy(CLIcmddata_map.key,
+            FPS_app_info_map.cmdkey,
+            sizeof(CLIcmddata_map.key)
+            - 1);
+    strncpy(CLIcmddata_map.description,
+            FPS_app_info_map.description,
+            sizeof(
+                CLIcmddata_map
+                .description) - 1);
+    if (CLIcmddata_map.cmdsettings
+        == NULL) {
+        CLIcmddata_map.cmdsettings =
+            &cms_map;
     }
 }
 
-/** @brief CLI function for AOloopControl_testPredictiveFilter */
-errno_t AOloopControl_PredictiveControl_testPredictiveFilter_cli()
+static errno_t compute_map()
 {
-    if(CLI_checkarg(1, 4) + CLI_checkarg(2, 2) + CLI_checkarg(3, 1) +
-            CLI_checkarg(4, 2) + CLI_checkarg(5, 3) ==
-            0)
-    {
-        AOloopControl_PredictiveControl_testPredictiveFilter(
-            data.cmdargtoken[1].val.string,
-            data.cmdargtoken[2].val.numl,
-            data.cmdargtoken[3].val.numf,
-            data.cmdargtoken[4].val.numl,
-            data.cmdargtoken[5].val.string,
-            1e-10);
+    AOloopControl_PredictiveControl_mapPredictiveFilter(
+        p_modecoeff, p_modeout,
+        p_delayfr);
+    return RETURN_SUCCESS;
+}
 
-        return CLICMD_SUCCESS;
-    }
-    else
-    {
-        return CLICMD_INVALID_ARG;
+
+/* =============================================================
+ *  CMD 3: aolmkpfilt (5 args)
+ * ============================================================ */
+
+static FPS_APP_INFO FPS_app_info_mk = {
+    .fps_name    = "aolmkpfilt",
+    .cmdkey      = "aolmkpfilt",
+    .description =
+        "test predictive filter"
+};
+
+#define FPS_PARAMS_MK(X) \
+    X(".trname", p_modecoeff, \
+      FPTYPE_STREAMNAME, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "trace image") \
+    X(".modeout", &p_modeout, \
+      FPTYPE_INT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "mode number") \
+    X(".delayfr", &p_delayfr, \
+      FPTYPE_FLOAT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "delay in frames") \
+    X(".filtsize", &p_filtsize, \
+      FPTYPE_INT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "filter size") \
+    X(".filtname", p_filtname, \
+      FPTYPE_STRING, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "output filter name")
+
+static CLICMDDATA CLIcmddata_mk = {
+    "", "", CLICMD_FIELDS_NOPARAM
+};
+static CMDSETTINGS cms_mk = {0};
+
+static __attribute__((constructor))
+void init_cms_mk(void)
+{
+    strncpy(CLIcmddata_mk.key,
+            FPS_app_info_mk.cmdkey,
+            sizeof(CLIcmddata_mk.key)
+            - 1);
+    strncpy(CLIcmddata_mk.description,
+            FPS_app_info_mk.description,
+            sizeof(
+                CLIcmddata_mk
+                .description) - 1);
+    if (CLIcmddata_mk.cmdsettings
+        == NULL) {
+        CLIcmddata_mk.cmdsettings =
+            &cms_mk;
     }
 }
 
-errno_t AOloopControl_PredictiveControl_setPFsimpleAve_cli()
+static errno_t compute_mk()
 {
-    if(CLI_checkarg(1, 4) + CLI_checkarg(2, 1) == 0)
-    {
-        AOloopControl_PredictiveControl_setPFsimpleAve(
-            data.cmdargtoken[1].val.string,
-            data.cmdargtoken[2].val.numf);
+    AOloopControl_PredictiveControl_testPredictiveFilter(
+        p_modecoeff, p_modeout,
+        p_delayfr, p_filtsize,
+        p_filtname, 1e-10);
+    return RETURN_SUCCESS;
+}
 
-        return CLICMD_SUCCESS;
-    }
-    else
-    {
-        return CLICMD_INVALID_ARG;
+
+/* =============================================================
+ *  CMD 4: aolpfsetave (2 args)
+ * ============================================================ */
+
+static FPS_APP_INFO FPS_app_info_ave = {
+    .fps_name    = "aolpfsetave",
+    .cmdkey      = "aolpfsetave",
+    .description =
+        "set PF to integrator"
+};
+
+#define FPS_PARAMS_AVE(X) \
+    X(".pfname", p_pfname, \
+      FPTYPE_STREAMNAME, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "predictive filter") \
+    X(".decaycoeff", &p_decaycoeff, \
+      FPTYPE_FLOAT64, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "decay coefficient")
+
+static CLICMDDATA CLIcmddata_ave = {
+    "", "", CLICMD_FIELDS_NOPARAM
+};
+static CMDSETTINGS cms_ave = {0};
+
+static __attribute__((constructor))
+void init_cms_ave(void)
+{
+    strncpy(CLIcmddata_ave.key,
+            FPS_app_info_ave.cmdkey,
+            sizeof(CLIcmddata_ave.key)
+            - 1);
+    strncpy(CLIcmddata_ave.description,
+            FPS_app_info_ave.description,
+            sizeof(
+                CLIcmddata_ave
+                .description) - 1);
+    if (CLIcmddata_ave.cmdsettings
+        == NULL) {
+        CLIcmddata_ave.cmdsettings =
+            &cms_ave;
     }
 }
 
-/* ===============================================================================================
- */
-/* ===============================================================================================
- */
-/*                                    FUNCTIONS SOURCE CODE */
-/* ===============================================================================================
- */
-/* ===============================================================================================
- */
-/** @name AOloopControl_PredictiveControl functions */
+static errno_t compute_ave()
+{
+    AOloopControl_PredictiveControl_setPFsimpleAve(
+        p_pfname, p_decaycoeff);
+    return RETURN_SUCCESS;
+}
+
+
+/* =============================================================
+ *  REGISTRATION
+ * ============================================================ */
+
+static errno_t CLIfunction_main(void)
+{
+    return safe_fps_generic_CLIfunction(
+        &FPS_app_info, farg, &CLIcmddata,
+        my_bindings, nb_bindings,
+        compute_function);
+}
+
+static FPS_CLI_BINDING bindings_map[] = {
+    FPS_PARAMS_MAP(FPS_X_BINDING)
+};
+static CLICMDARGDEF farg_map[] = {
+    FPS_PARAMS_MAP(FPS_X_FARG)
+};
+
+static errno_t CLIfunction_map(void)
+{
+    return safe_fps_generic_CLIfunction(
+        &FPS_app_info_map,
+        farg_map, &CLIcmddata_map,
+        bindings_map,
+        sizeof(bindings_map) /
+        sizeof(FPS_CLI_BINDING),
+        compute_map);
+}
+
+static FPS_CLI_BINDING bindings_mk[] = {
+    FPS_PARAMS_MK(FPS_X_BINDING)
+};
+static CLICMDARGDEF farg_mk[] = {
+    FPS_PARAMS_MK(FPS_X_FARG)
+};
+
+static errno_t CLIfunction_mk(void)
+{
+    return safe_fps_generic_CLIfunction(
+        &FPS_app_info_mk,
+        farg_mk, &CLIcmddata_mk,
+        bindings_mk,
+        sizeof(bindings_mk) /
+        sizeof(FPS_CLI_BINDING),
+        compute_mk);
+}
+
+static FPS_CLI_BINDING bindings_ave[] = {
+    FPS_PARAMS_AVE(FPS_X_BINDING)
+};
+static CLICMDARGDEF farg_ave[] = {
+    FPS_PARAMS_AVE(FPS_X_FARG)
+};
+
+static errno_t CLIfunction_ave(void)
+{
+    return safe_fps_generic_CLIfunction(
+        &FPS_app_info_ave,
+        farg_ave, &CLIcmddata_ave,
+        bindings_ave,
+        sizeof(bindings_ave) /
+        sizeof(FPS_CLI_BINDING),
+        compute_ave);
+}
 
 static errno_t init_module_CLI()
 {
+    safe_fps_fill_farg_examples(
+        farg, my_bindings, nb_bindings);
+    safe_fps_fill_farg_examples(
+        farg_map, bindings_map,
+        sizeof(bindings_map) /
+        sizeof(FPS_CLI_BINDING));
+    safe_fps_fill_farg_examples(
+        farg_mk, bindings_mk,
+        sizeof(bindings_mk) /
+        sizeof(FPS_CLI_BINDING));
+    safe_fps_fill_farg_examples(
+        farg_ave, bindings_ave,
+        sizeof(bindings_ave) /
+        sizeof(FPS_CLI_BINDING));
 
-    /* ===============================================================================================
-    */
-    /* ===============================================================================================
-    */
-    /** @name AOloopControl_PredictiveControl - 1. PREDICTIVE CONTROL */
-    /* ===============================================================================================
-    */
-    /* ===============================================================================================
-    */
-
-    RegisterCLIcommand(
-        "aolPFwatchin",
-        __FILE__,
-        AOloopControl_PredictiveControl_builPFloop_WatchInput_cli,
-        "watch telemetry for predictive filter input",
-        "<loop #> <PFblock #> <start> <end>",
-        "aolPFwatchin 0 2",
-        "long AOloopControl_builPFloop_WatchInput(long loop, long PFblock, "
-        "long PFblockStart, long "
-        "PFblockEnd, long NBbuff)");
-
-    RegisterCLIcommand("aolmappfilt",
-                       __FILE__,
-                       AOloopControl_PredictiveControl_mapPredictiveFilter_cli,
-                       "map/search predictive filter",
-                       "<input coeffs> <mode number> <delay [frames]>",
-                       "aolmkapfilt coeffim 23 2.4",
-                       "long AOloopControl_mapPredictiveFilter(char "
-                       "*IDmodecoeff_name, long modeout, double delayfr)");
-
-    RegisterCLIcommand("aolmkpfilt",
-                       __FILE__,
-                       AOloopControl_PredictiveControl_testPredictiveFilter_cli,
-                       "test predictive filter",
-                       "<trace im> <mode number> <delay [frames]> <filter "
-                       "size> <out filter name>",
-                       "aolmkpfilt traceim 23 2.4 20 filt23",
-                       "long AOloopControl_testPredictiveFilter(char "
-                       "*IDtrace_name, long mode, double delayfr, long "
-                       "filtsize, char *IDfilt_name, double SVDeps)");
-
-    RegisterCLIcommand(
-        "aolpfsetave",
-        __FILE__,
-        AOloopControl_PredictiveControl_setPFsimpleAve_cli,
-        "set predictive filter to integrator",
-        "<PredFilter> <DecayCoeff>",
-        "aolpfsetave outPFb0 0.5",
-        "long AOloopControl_PredictiveControl_setPFsimpleAve(char *IDPF_name, "
-        "float DecayCoeff)");
-
-    // add atexit functions here
-    // atexit((void*) myfunc);
+    {
+        int cmdi = RegisterCLIcmd(
+            CLIcmddata,
+            CLIfunction_main);
+        CLIcmddata.cmdsettings =
+            &data.cmd[cmdi].cmdsettings;
+    }
+    {
+        int cmdi = RegisterCLIcmd(
+            CLIcmddata_map,
+            CLIfunction_map);
+        CLIcmddata_map.cmdsettings =
+            &data.cmd[cmdi].cmdsettings;
+    }
+    {
+        int cmdi = RegisterCLIcmd(
+            CLIcmddata_mk,
+            CLIfunction_mk);
+        CLIcmddata_mk.cmdsettings =
+            &data.cmd[cmdi].cmdsettings;
+    }
+    {
+        int cmdi = RegisterCLIcmd(
+            CLIcmddata_ave,
+            CLIfunction_ave);
+        CLIcmddata_ave.cmdsettings =
+            &data.cmd[cmdi].cmdsettings;
+    }
 
     return RETURN_SUCCESS;
 }
--- computeCalib/computeCalib.c
+++ computeCalib/computeCalib.c
@@ -1,29 +1,19 @@
 /**
  * @file    AOloopControl_computeCalib.c
- * @brief   Adaptive Optics Control loop engine compute calibration
+ * @brief   AO loop compute calibration
  *
- * AO engine uses stream data structure
- *
- *
- *
+ * Uses FPS V2 framework.
  */
 
-// module default short name
-// all CLI calls to this module functions will be <shortname>.<funcname>
-// if set to "", then calls use <funcname>
 #define MODULE_SHORTNAME_DEFAULT "cacaocc"
-
-// Module short description
-#define MODULE_DESCRIPTION "AO loop control compute calibration"
-
-// Application to which module belongs
+#define MODULE_DESCRIPTION \
+    "AO loop control compute calibration"
 #define MODULE_APPLICATION "cacao-cli"
 
 #define _GNU_SOURCE
 
-
-
 #include "CLIcore/CLIcore.h"
+#include "fps.h"
 
 #include "actmap_sample2D.h"
 #include "compute_control_modes.h"
@@ -38,43 +28,113 @@
 #include "computeCalib.h"
 
 
-
-
 INIT_MODULE_LIB(AOloopControl_computeCalib)
 
-/** @brief CLI function for AOloopControl_Hadamard_decodeRM */
-errno_t AOloopControl_computeCalib_Hadamard_decodeRM_cli()
-{
-    if(CLI_checkarg(1, 4) + CLI_checkarg(2, 4) + CLI_checkarg(3, 4) +
-            CLI_checkarg(4, 3) ==
-            0)
-    {
-        AOloopControl_computeCalib_Hadamard_decodeRM(
-            data.cmdargtoken[1].val.string,
-            data.cmdargtoken[2].val.string,
-            data.cmdargtoken[3].val.string,
-            data.cmdargtoken[4].val.string);
 
-        return CLICMD_SUCCESS;
-    }
-    else
-    {
-        return CLICMD_INVALID_ARG;
+/* =============================================================
+ *  CMD 1: aolHaddec (4 args)
+ * ============================================================ */
+
+static char p_inrm[
+    FUNCTION_PARAMETER_STRMAXLEN]
+    = "imRMh";
+static char p_hmat[
+    FUNCTION_PARAMETER_STRMAXLEN]
+    = "Hmat";
+static char p_pixidx[
+    FUNCTION_PARAMETER_STRMAXLEN]
+    = "pixiind";
+static char p_outrm[
+    FUNCTION_PARAMETER_STRMAXLEN]
+    = "imRM";
+
+static FPS_APP_INFO FPS_app_info = {
+    .fps_name    = "aolHaddec",
+    .cmdkey      = "aolHaddec",
+    .description =
+        "decode Hadamard matrix"
+};
+
+#define FPS_PARAMS(X) \
+    X(".inrm", p_inrm, \
+      FPTYPE_STREAMNAME, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "input RM") \
+    X(".hmat", p_hmat, \
+      FPTYPE_STREAMNAME, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "Hadamard matrix") \
+    X(".pixidx", p_pixidx, \
+      FPTYPE_STREAMNAME, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "DM pixel index frame") \
+    X(".outrm", p_outrm, \
+      FPTYPE_STRING, 1, \
+      FPFLAG_DEFAULT_INPUT, \
+      "output RM")
+
+static FPS_CLI_BINDING my_bindings[] = {
+    FPS_PARAMS(FPS_X_BINDING)
+};
+static const int nb_bindings =
+    sizeof(my_bindings) /
+    sizeof(FPS_CLI_BINDING);
+
+static CLICMDARGDEF farg[] = {
+    FPS_PARAMS(FPS_X_FARG)
+};
+
+static CLICMDDATA CLIcmddata = {
+    "", "", CLICMD_FIELDS_DEFAULTS
+};
+static CMDSETTINGS cms = {0};
+
+static __attribute__((constructor))
+void init_cms(void)
+{
+    strncpy(CLIcmddata.key,
+            FPS_app_info.cmdkey,
+            sizeof(CLIcmddata.key) - 1);
+    strncpy(CLIcmddata.description,
+            FPS_app_info.description,
+            sizeof(CLIcmddata.description)
+            - 1);
+    if (CLIcmddata.cmdsettings == NULL) {
+        CLIcmddata.cmdsettings = &cms;
     }
 }
 
+static errno_t compute_function()
+{
+    AOloopControl_computeCalib_Hadamard_decodeRM(
+        p_inrm, p_hmat,
+        p_pixidx, p_outrm);
+    return RETURN_SUCCESS;
+}
+
+static errno_t CLIfunction(void)
+{
+    return safe_fps_generic_CLIfunction(
+        &FPS_app_info, farg, &CLIcmddata,
+        my_bindings, nb_bindings,
+        compute_function);
+}
+
+
+/* =============================================================
+ *  REGISTRATION
+ * ============================================================ */
+
 static errno_t init_module_CLI()
 {
-    RegisterCLIcommand(
-        "aolHaddec",
-        __FILE__,
-        AOloopControl_computeCalib_Hadamard_decodeRM_cli,
-        "decode Hadamard matrix",
-        "<input RM> <Hadamard matrix> <DMpix index frame> <output RM>",
-        "aolHaddec imRMh Hmat pixiind imRM",
-        "long AOloopControl_computeCalib_Hadamard_decodeRM(char *inname, char "
-        "*Hmatname, char "
-        "*indexname, char *outname)");
+    safe_fps_fill_farg_examples(
+        farg, my_bindings, nb_bindings);
+    {
+        int cmdi = RegisterCLIcmd(
+            CLIcmddata, CLIfunction);
+        CLIcmddata.cmdsettings =
+            &data.cmd[cmdi].cmdsettings;
+    }
 
     CLIADDCMD_cacao_computeCalib__compute_control_modes();
 
@@ -93,10 +153,6 @@ static errno_t init_module_CLI()
     CLIADDCMD_AOloopControl_computeCalib__mkCMsvd();
 
     CLIADDCMD_AOloopControl_computeCalib__sample2D();
-
-
-    // add atexit functions here
-    // atexit((void*) myfunc);
 
     return RETURN_SUCCESS;
 }
